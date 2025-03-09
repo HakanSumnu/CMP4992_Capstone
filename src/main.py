2d198fecc4e8632638a63ebc2ba8317369fdb502
HakanSumnu <karus1560@hotmail.com>
2025-03-09 18:09:53 +0300

    Çoğu değişkenin tipini ekledim.

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import numpy as np
 import csv
 import math
 
-DISTANCE_BETWEEN_ROBOT_INITIAL_AND_CAMERA = 10.0 # In cm. It should be added to line equation since we use right hand-side coordinate system, which causes robot's initisl point to stay at
+DISTANCE_BETWEEN_ROBOT_INITIAL_AND_CAMERA: float = 10.0 # In cm. It should be added to line equation since we use right hand-side coordinate system, which causes robot's initisl point to stay at
                                                  # some point whose z component is negative. Therefore, every point in the coordinate system should be shifted this amount in Z axis 
                                                  # to locate robot's initial point to origin (0, 0).
-directionVector = np.array([0, 0, -1]) # Before I change my mathematical formulation, this vector's z componet had to be -1. Now, it does not matter whether it is -1 or 1. 
+directionVector: np.ndarray = np.array([0, 0, -1]) # Before I change my mathematical formulation, this vector's z componet had to be -1. Now, it does not matter whether it is -1 or 1. 
                                        # The only thing that matter is that this vector should be a unit vector and all of its components must be zero except it z component.
 
 # Opening test file and collecting XYZ compenets of the dots and storing these components in separate arrays.
@@ -24,19 +24,19 @@ Y = np.array(Y)
 Z = np.array(Z)
 
 # Calculating best fit line in 3D.
-A = np.array([[np.dot(X, X), np.sum(X)], [np.sum(X), X.shape[0]]])
-B1 = np.array([np.dot(X, Y), np.sum(Y)])
-x1 = np.linalg.solve(A, B1)
+A: np.ndarray = np.array([[np.dot(X, X), np.sum(X)], [np.sum(X), X.shape[0]]])
+B1: np.ndarray = np.array([np.dot(X, Y), np.sum(Y)])
+x1: np.ndarray = np.linalg.solve(A, B1)
 print(f"Equation of y: {x1}")
 
-B2 = np.array([np.dot(X, Z), np.sum(Z)])
-x2 = np.linalg.solve(A, B2)
+B2: np.ndarray = np.array([np.dot(X, Z), np.sum(Z)])
+x2: np.ndarray = np.linalg.solve(A, B2)
 print(f"Equation of z: {x2}")
 
 # Calculating the angle between XZ plane at y = 0 and YZ components of the vector that is parallel to best fit line.
-parallelVectorToLineClamppedToYZ = np.array([0, x1[0], x2[0]])
+parallelVectorToLineClamppedToYZ: np.ndarray = np.array([0, x1[0], x2[0]])
 parallelVectorToLineClamppedToYZ /= np.linalg.norm(parallelVectorToLineClamppedToYZ)
-angle = math.acos(np.dot(parallelVectorToLineClamppedToYZ, directionVector))
+angle: float = math.acos(np.dot(parallelVectorToLineClamppedToYZ, directionVector))
 
 if angle >= math.pi - angle:
     angle = math.pi - angle
@@ -63,5 +63,5 @@ print(f"Angle: {angle}")
 #print(f"Constants for components after the rotation: {constantForComponents}")
 
 # Rotating the best fit line and considering it in XZ coordinate system
-coefficientsOfNewLine = np.array([math.sin(angle) * x1[0] + math.cos(angle) * x2[0], math.sin(angle) * x1[1] + math.cos(angle) * x2[1]])
+coefficientsOfNewLine: np.ndarray = np.array([math.sin(angle) * x1[0] + math.cos(angle) * x2[0], math.sin(angle) * x1[1] + math.cos(angle) * x2[1]])
 print(f"Coefficients of the new line: {coefficientsOfNewLine}")
